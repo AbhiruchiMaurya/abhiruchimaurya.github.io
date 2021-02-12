d5f994631e9773a28e0d6868e1b49fa32880d5d7
Abhiruchi Maurya <abhiruchimaurya@outlook.com>
2021-02-12 21:40:56 +0530

    code clean up

--- js/custom.js
+++ js/custom.js
@@ -7,26 +7,38 @@ $(window).on("load", function () {
   $("#preloader").delay(1000).fadeOut("slow");
 
   let header = () => {
-  let text = "HI, I'M ABHIRUCHI MAURYA";
+    let text = "HI, I'M ABHIRUCHI MAURYA";
 
-  let textElements = text.split("").map(function (c) {
-    return $('<span id="' + c + '">' + c + "</span>");
-  });
+    let textElements = text.split("").map(function (c) {
+      return $('<span id="' + c + '">' + c + "</span>");
+    });
 
-  let el = $(".heading-1-banner");
-  let delay = 300; // Tune this for different letter delays.
-  textElements.forEach(function (e, i) {
-    el.append(e);
-    e.hide();
-    setTimeout(function () {
-      e.fadeIn(300);
-    }, 300 + i * delay);
-  });
-  
-  }
+    let el = $(".heading-1-banner");
+    let delay = 300; // Tune this for different letter delays.
+    textElements.forEach(function (e, i) {
+      el.append(e);
+      e.hide();
+      setTimeout(function () {
+        e.fadeIn(300);
+      }, 300 + i * delay);
+    });
+  };
 
   header();
 
+  $(document).keydown(function (event) {
+    if (event.keyCode == 123) {
+      // Prevent F12
+      return false;
+    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+      // Prevent Ctrl+Shift+I
+      return false;
+    }
+  });
+
+  $(document).on("contextmenu", function (e) {
+    e.preventDefault();
+  });
 });
 
 $(document).ready(function () {
